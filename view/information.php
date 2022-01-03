1b5c91c50da39961085e7d3e42350aae1893a091
scourtois <simon.courtois@etudiant.univ-rennes1.fr>
2022-01-03 10:04:27 +0100

    ajout des cotes v2

--- view/information.php
+++ view/information.php
@@ -1,24 +1,25 @@
 <?php
+if(isset($_POST['information'])){
  require("../Controler/connexionBDD.php");
- require("../Models/Match.class.php");
 $reponse = $db->query('SELECT * FROM Matchs where id_match = '.$_POST['match'].'');
 $reponse->execute(); 
-while ($match = $reponse->fetch())    //fetch() itérateur
-{
-    $m = new Match();
-    $m->create($match['id_sport'],$match['equipe1'],$match['equipe2'],$match['date'],$match['cote']);
-echo '<table>';
-    echo '<thead>';
-        echo '<tr>';
-            echo '<th colspan="2">'.$m->__toString().'</th>';
-        echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
-        echo '<tr>';
-            echo '<td colspan="2">'."Cote :"." ".$match['cote'].'</td>';
-        echo '</tr>';
-    echo '</tbody>';
-echo '</table>';
+    while ($match = $reponse->fetch())    //fetch() itérateur
+    {
+    echo '<table>';
+        echo '<thead>';
+            echo '<tr>';
+                echo '<th colspan="2">'.$match['equipe1']." vs ".$match['equipe2'].'</th>';
+            echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+            echo '<tr>';
+                echo '<td colspan="2">'."Cote victoire de ".$match['equipe1']." :"." ".$match['cotev1'].'</td>';
+                echo '<td colspan="2">'."Cote victoire de ".$match['equipe2']." :"." ".$match['cotev2'].'</td>';
+                echo '<td colspan="2">'."Cote match nul "." :"." ".$match['coteNul'].'</td>';
+            echo '</tr>';
+        echo '</tbody>';
+    echo '</table>';
+    }
 }
 ?>
 
